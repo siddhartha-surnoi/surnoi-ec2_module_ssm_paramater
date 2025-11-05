05fd12c9e91d8d90c2eb0229762451036a6e8c9b
siddhartha-surnoi <kanaparthisiddhartha@surnoi.com>
2025-11-05 17:37:08 +0530

    Update variables.tf

--- customize-ec2-module/variables.tf
+++ customize-ec2-module/variables.tf
@@ -129,13 +129,9 @@ variable "instances" {
 
     java-agent-1 = {
       instance_type         = "t3a.small"
-      iam_instance_profile  = null
+      iam_instance_profile  = "IAM-ECR-Role"
       user_data             = "user_data/user_data.backend.sh"
       security_group_ref    = "java_securitygroup"
     }
-
-
-
-   
   }
 }
